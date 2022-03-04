1d365ed9eedb4bbd21ecf58d76fd98ce07588224
Hannah Malm <hannahemalm616@gmail.com>
2022-03-04 09:47:08 -0600

    commit tree 851926e2309a19d916fc7c030eed465d9cafaabb

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,7 @@
 export function managePresents(state, action){
+    action = {
+        type: "INCREASE",
+      };
 }
+
+//If the reducer receives a type set to "INCREASE", return a new state where the value of numberOfPresents is increased by one.
